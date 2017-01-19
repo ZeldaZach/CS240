bca0e972eac44b6318c7bb3394ad9e69c2669fd2
Zach  H <zoosorbs@gmail.com>
2017-01-19 13:31:40 -0500

    lab1

---
+++ lab1/lab1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// Our first program. Print out two phrases then exit.
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "C++ Data Structures!" << std::endl;
+
+	return 0;
+}
