8374fba9190b8f849c71ef51b9e1ecdd70a6a12a
Zach  H <zoosorbs@gmail.com>
2017-02-08 13:56:18 -0500

    class

---
+++ classwork/020817/List.h
@@ -0,0 +1,13 @@
+#ifndef LIST_H
+#define LIST_H
+
+class List
+{
+	private:
+		Node *head;
+
+	public:
+		List();
+};
+
+#endif
---
+++ classwork/020817/Node.h
@@ -0,0 +1,8 @@
+class Node
+{
+	public:
+		Node *next;
+		int data;
+};
+
+
